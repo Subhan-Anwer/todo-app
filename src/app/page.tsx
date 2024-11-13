3f9d228802a271ef73b86aec69872db52063aa2d
Subhan-anwer <sheikhanwer30@gmail.com>
2024-11-13 10:41:58 +0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,73 @@
-import Image from "next/image";
-
+'use client'
+import Header from "@/components/header";
+import { useState, FormEvent } from "react";
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [title, settitle] = useState<string>('');
+  // const [mainTask, setMainTask] = useState<Task[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  let listContainer = document.getElementById('list-container');
+
+  const addTask = () => {
+    if (title === '') {
+      alert('PLease enter a task'); 
+    } else {
+      let li = document.createElement("li");
+      li.innerHTML = `${title} <span></span>`; 
+      listContainer?.appendChild(li);   
+    }
+  }
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();                                  // Preventing the page from refreshing
+    // setMainTask([...mainTask, { title }]);
+    settitle('');                                        // Clearing the input field
+    addTask();
+  }
+
+  listContainer?.addEventListener('click', (e: any) => {
+    const target = e.target as HTMLElement;
+    if (target.tagName === "LI") {
+      target.classList.add("checked");
+    } else if (target.tagName === "LI" && target.classList.contains("checked")) {
+      target.classList.remove("checked");
+    } else if (target.tagName === "SPAN") {
+      target.parentElement?.remove();
+    }    
+  })
+
+  
+
+
+  
+  return (
+    <div>
+      <Header/>
+      <div className="flex flex-col w-full max-w-[540px] bg-[#212121] text-[#b8b8b8] mt-[100px] mx-auto mb-[20px] pt-[40px] px-[30px] pb-[70px] rounded-[15px]">
+
+        
+        <form onSubmit={submitHandler} className="flex justify-center w-[90%] mx-auto mb-[50px]">
+          <input type="text" id="input-box" placeholder="Write your task here" className="h-[50px] w-[400px] rounded-[60px] rounded-r-[0px] border-[3px] border-r-0 border-solid border-[#303030] bg-[#161616] px-[20px] active:bg-outline-none focus:outline-none" value={title} onChange={(e) => {
+            settitle(e.target.value)
+          }}/>
+          <button className="h-[50px] w-[80px] bg-[#303030] rounded-r-[50px] z-[1] active:bg-outline-none focus:outline-none">Add</button>
+        </form>
+
+
+
+        <ul id="list-container">
+          <li className="checked">Task 1 Task 1 Task 1 Task 1 Task 1 Tas ka s s <span></span></li>
+        </ul>
+
+
+      </div>
     </div>
   );
+
+
+
+
+
+
+
+
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="bg-[#212121] h-[120px] flex items-center justify-center">
+      <h1 className="text-[#fff] text-[80px] text-center">To Do List</h1>
+    </div>
+  )
+}
+
+export default Header
