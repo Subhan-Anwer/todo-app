6bc145b31d92acd352d8e0b123106d5a71f5baab
Subhan-anwer <sheikhanwer30@gmail.com>
2024-11-14 10:30:52 +0500

    14 Nov

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,13 @@
 'use client'
+
 import Header from "@/components/header";
-import { useState, FormEvent } from "react";
-export default function Home() {
-  const [title, settitle] = useState<string>('');
-  // const [mainTask, setMainTask] = useState<Task[]>([]);
+import TodoList from "@/components/todolist";
 
-  const listContainer = document.getElementById('list-container');
-
-  const addTask = () => {
-    if (title === '') {
-      alert('PLease enter a task'); 
-    } else {
-      const li = document.createElement("li");
-      li.innerHTML = `${title} <span></span>`; 
-      listContainer?.appendChild(li);   
-    }
-  }
-
-  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();                                  // Preventing the page from refreshing
-    // setMainTask([...mainTask, { title }]);
-    settitle('');                                        // Clearing the input field
-    addTask();
-  }
-
-  listContainer?.addEventListener('click', (e: any) => {
-    const target = e.target as HTMLElement;
-    if (target.tagName === "LI") {
-      target.classList.add("checked");
-    } else if (target.tagName === "LI" && target.classList.contains("checked")) {
-      target.classList.remove("checked");
-    } else if (target.tagName === "SPAN") {
-      target.parentElement?.remove();
-    }    
-  })
-
-  
-
-
-  
+export default function Home() {  
   return (
     <div>
       <Header/>
-      <div className="flex flex-col w-full max-w-[540px] bg-[#212121] text-[#b8b8b8] mt-[100px] mx-auto mb-[20px] pt-[40px] px-[30px] pb-[70px] rounded-[15px]">
-
-        
-        <form onSubmit={submitHandler} className="flex justify-center w-[90%] mx-auto mb-[50px]">
-          <input type="text" id="input-box" placeholder="Write your task here" className="h-[50px] w-[400px] rounded-[60px] rounded-r-[0px] border-[3px] border-r-0 border-solid border-[#303030] bg-[#161616] px-[20px] active:bg-outline-none focus:outline-none" value={title} onChange={(e) => {
-            settitle(e.target.value)
-          }}/>
-          <button className="h-[50px] w-[80px] bg-[#303030] rounded-r-[50px] z-[1] active:bg-outline-none focus:outline-none">Add</button>
-        </form>
-
-
-
-        <ul id="list-container">
-          <li className="checked">Task 1 Task 1 Task 1 Task 1 Task 1 Tas ka s s <span></span></li>
-        </ul>
-
-
-      </div>
+      <TodoList/>
     </div>
   );
-
-
-
-
-
-
-
-
 }
---
+++ src/components/todolist.tsx
@@ -0,0 +1,69 @@
+'use client'
+import { useState } from "react"
+
+interface Todo {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+const TodoList = () => {
+
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [inputValue, setInputValue] = useState("");
+
+    const handleAddTask = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (inputValue.trim()) {
+            const newTask = {
+                id: Date.now(),
+                text: inputValue,
+                completed: false,
+            };
+            setTodos([...todos, newTask]);
+            setInputValue("");
+        }
+    };
+
+    const handleDeleteTask = (id: number) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    };
+
+    const handleToggleComplete = (id: number) => {
+        setTodos(todos.map(todo =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        ));
+    };
+
+    return (
+        <div className="flex flex-col w-full max-w-[540px] bg-[#212121] text-[#b8b8b8] mt-[100px] mx-auto mb-[20px] pt-[40px] px-[30px] pb-[70px] rounded-[15px]">
+            <form className="flex justify-center w-[90%] mx-auto mb-[50px]">
+                <input type="text" placeholder="Write your task here" className="h-[50px] w-[400px] rounded-[60px] rounded-r-[0px] border-[3px] border-r-0 border-solid border-[#303030] bg-[#161616] px-[20px] active:bg-outline-none focus:outline-none" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+                <button onClick={handleAddTask} className="h-[50px] w-[80px] bg-[#303030] rounded-r-[50px] z-[1] active:bg-outline-none focus:outline-none">Add</button>
+            </form>
+            <ul id="list-container">
+
+                {todos.map(todo => (
+                    <li
+                        key={todo.id}
+                        className={todo.completed ? "checked" : ""}
+                        onClick={() => handleToggleComplete(todo.id)}
+                    >
+                        {todo.text}
+                        <span
+                            onClick={(e) => {
+                                e.stopPropagation(); // Prevent marking task as complete
+                                handleDeleteTask(todo.id);
+                            }}>
+                        </span>
+                    </li>
+                ))}
+
+
+
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
