a8cd24930d976c3d0dd79c1daf4d3d3e32dc5437
Subhan-anwer <sheikhanwer30@gmail.com>
2024-11-15 17:10:55 +0500

    responsiveness done

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className="bg-[#212121] h-[120px] flex items-center justify-center">
-      <h1 className="sm:text-[80px]  text-[#fff] text-[50px] text-center ">To Do List</h1>
+      <h1 className="sm:text-[80px]  text-[#fff] text-[50px] text-center select-none">To Do List</h1>
     </div>
   )
 }
--- src/components/todolist.tsx
+++ src/components/todolist.tsx
@@ -38,13 +38,13 @@ const TodoList = () => {
     return (
         <div className="flex flex-col w-full max-w-[285px] sm:max-w-[540px] bg-[#212121] text-[#b8b8b8] mt-[100px] mx-auto mb-[20px] pt-[40px] px-[30px] pb-[70px] rounded-[15px]">
             <form className="flex flex-col items-center sm:flex-row sm:justify-center sm:w-[90%] sm:mx-auto sm:mb-[50px]">
-                <input type="text" placeholder="Write your task here" 
-                className="h-[50px] w-[250px] rounded-[60px] rounded-r-[0px] border-[3px] border-r-0 border-solid border-[#303030] bg-[#161616] px-[20px] active:bg-outline-none focus:outline-none 
-                            sm:w-[400px]  " 
-                value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
-                <button onClick={handleAddTask} className="h-[50px] w-[80px] bg-[#303030] rounded-r-[50px] z-[1] active:bg-outline-none focus:outline-none">Add</button>
+                <input type="text" placeholder="Write your task here"
+                    className="h-[50px] w-[250px] sm:w-[400px] rounded-[60px] sm:rounded-r-[0px] border-[3px] sm:border-r-0 border-solid border-[#303030] bg-[#161616] px-[20px] active:bg-outline-none focus:outline-none"
+                    value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                <button onClick={handleAddTask}
+                    className="sm:h-[50px] h-[45px] w-[250px] sm:w-[80px] bg-[#303030] sm:rounded-l-[0] rounded-[50px] sm:m-0 m-2 z-[1] active:bg-outline-none focus:outline-none">Add</button>
             </form>
-            <ul id="list-container">
+            <ul id="list-container" className="sm:w-[445px] w-[250px] self-center sm:mt-0 mt-5">
 
                 {todos.map(todo => (
                     <li
@@ -54,6 +54,7 @@ const TodoList = () => {
                     >
                         {todo.text}
                         <span
+                            className=""
                             onClick={(e) => {
                                 e.stopPropagation(); // Prevent marking task as complete
                                 handleDeleteTask(todo.id);
