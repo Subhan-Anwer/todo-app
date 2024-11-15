8aea35c6192f7c86190d4801e9740ece3c2eda57
Subhan-anwer <sheikhanwer30@gmail.com>
2024-11-15 16:15:35 +0500

    responsiveness

--- src/components/todolist.tsx
+++ src/components/todolist.tsx
@@ -36,9 +36,12 @@ const TodoList = () => {
     };
 
     return (
-        <div className="flex flex-col w-full max-w-[540px] bg-[#212121] text-[#b8b8b8] mt-[100px] mx-auto mb-[20px] pt-[40px] px-[30px] pb-[70px] rounded-[15px]">
-            <form className="flex justify-center w-[90%] mx-auto mb-[50px]">
-                <input type="text" placeholder="Write your task here" className="h-[50px] w-[400px] rounded-[60px] rounded-r-[0px] border-[3px] border-r-0 border-solid border-[#303030] bg-[#161616] px-[20px] active:bg-outline-none focus:outline-none" value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+        <div className="flex flex-col w-full max-w-[285px] sm:max-w-[540px] bg-[#212121] text-[#b8b8b8] mt-[100px] mx-auto mb-[20px] pt-[40px] px-[30px] pb-[70px] rounded-[15px]">
+            <form className="flex flex-col items-center sm:flex-row sm:justify-center sm:w-[90%] sm:mx-auto sm:mb-[50px]">
+                <input type="text" placeholder="Write your task here" 
+                className="h-[50px] w-[250px] rounded-[60px] rounded-r-[0px] border-[3px] border-r-0 border-solid border-[#303030] bg-[#161616] px-[20px] active:bg-outline-none focus:outline-none 
+                            sm:w-[400px]  " 
+                value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
                 <button onClick={handleAddTask} className="h-[50px] w-[80px] bg-[#303030] rounded-r-[50px] z-[1] active:bg-outline-none focus:outline-none">Add</button>
             </form>
             <ul id="list-container">
